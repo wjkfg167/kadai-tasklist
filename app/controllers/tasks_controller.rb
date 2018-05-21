e14d4778f6c2dfba840f8d4164d3bf43da4a9802
tech.free <techfree@outlook.jp>
2018-05-21 05:40:24 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,55 +1,56 @@
 class TasksController < ApplicationController
   def index
-    @messages = Message.all
+    @tasks = Task.all
   end
 
   def show
-    @message = Message.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
-    @message = Message.new
+    @task = Task.new
   end
 
   def create
-    @message = Message.new(message_params)
+    @task = Task.new(task_params)
 
-    if @message.save
-      flash[:success] = 'Message が正常に投稿されました'
-      redirect_to @message
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
     else
-      flash.now[:danger] = 'Message が投稿されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
 
   def edit
-    @message = Message.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
+    @task = Task.find(params[:id])
 
-    if @message.update(message_params)
-      flash[:success] = 'Message は正常に更新されました'
-      redirect_to @message
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
     else
-      flash.now[:danger] = 'Message は更新されませんでした'
+      flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
   end
 
- def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
 
-    flash[:success] = 'Message は正常に削除されました'
-    redirect_to messages_url
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
 
   private
 
   # Strong Parameter
-  def message_params
-    params.require(:message).permit(:content)
+  def task_params
+    params.require(:task).permit(:content)
   end
+end
